669390c611095973ede2600b8bcbf2cb842bf72b
sailestha28 <sailestha2018@gmail.com>
2021-10-13 18:27:09 +0545

    slider bug fixed

--- client/src/components/BannerCarousel.vue
+++ client/src/components/BannerCarousel.vue
@@ -7,11 +7,14 @@
                         <img :src="bannerPath + banner.image" alt="">
                     </div>
                     <div class="text-wrap">
-                        <h3 class="slide-title" v-if="banner.title">{{ banner.title }}</h3>
-                        <div class="slide-content" v-if="banner.excerpt">
-                            <p>{{ banner.excerpt }}</p>
+                        <div class="text-content-wrap">
+                          
+                            <h3 class="slide-title" v-if="banner.title">{{ banner.title }}</h3>
+                            <div class="slide-content" v-if="banner.excerpt">
+                                <p>{{ banner.excerpt }}</p>
+                            </div>
+                            <a class="slide-link btn btn-primary more" v-if="banner.button_text" :href="banner.button_link" target="_new">{{ banner.button_text }}</a>
                         </div>
-                        <a class="slide-link btn btn-primary more" v-if="banner.button_text" :href="banner.button_link" target="_new">{{ banner.button_text }}</a>
                     </div>
                 </div>
             </slide>
@@ -22,7 +25,6 @@
         </carousel>
     </section>
 </template>
-
 <script>
 import { mapGetters, mapActions } from "vuex";
 import 'vue3-carousel/dist/carousel.css';
@@ -40,28 +42,28 @@ export default {
                 itemsToShow: 1,
             },
             bannerPath: process.env.VUE_APP_BANNER_PATH,
-            slide: [{
-                image_source: require('@/assets/images/slider/slider-1.jpg'),
-                slide_title: "Slider title",
-                slide_content: "description text",
-                slide_link: "#slider-link"
-            }, {
-                image_source: require('@/assets/images/slider/slider-2.jpg'),
-                slide_title: "Slider title",
-                slide_content: "description text",
-                slide_link: "#slider-link"
-            }, {
-                image_source: require('@/assets/images/slider/slider-3.jpg'),
-                slide_title: "Slider title",
-                slide_content: "description text",
-                slide_link: "#slider-link"
-            }],
+            // slide: [{
+            //     image_source: require('@/assets/images/slider/slider-1.jpg'),
+            //     slide_title: "Slider title",
+            //     slide_content: "description text",
+            //     slide_link: "#slider-link"
+            // }, {
+            //     image_source: require('@/assets/images/slider/slider-2.jpg'),
+            //     slide_title: "Slider title",
+            //     slide_content: "description text",
+            //     slide_link: "#slider-link"
+            // }, {
+            //     image_source: require('@/assets/images/slider/slider-3.jpg'),
+            //     slide_title: "Slider title",
+            //     slide_content: "description text",
+            //     slide_link: "#slider-link"
+            // }],
         }
     },
     computed: {
         ...mapGetters("banner", ["banners"]),
     },
-    created(){
+    created() {
         this.fetchBanners()
     },
     methods: {
